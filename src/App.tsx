774e3f9ba45371cdc85437dc2d91dbfc91439f29
Mehmood <m.hassan.rahi@gmail.com>
2021-09-23 18:46:34 +0500

    * Added some basic style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Greet from "./components/Greet";
 
 function App() {
-    return <Greet name="Hassan" messageCount={10}/>
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: "100vh"}}>
+            <Greet name="Hassan" messageCount={10} isLoggedIn={true} />
+        </div>
+    );
 }
 
 export default App;
