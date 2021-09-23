2407cb495a296f96e41a6049b86b009ff5b8d6a3
Mehmood <m.hassan.rahi@gmail.com>
2021-09-23 16:53:27 +0500

    * Added Greet component

---
+++ src/components/Greet.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+    return (
+        <div>
+            Hi Mehmood! You have 100 unread messages.
+        </div>
+    )
+}
+
+export default Greet
