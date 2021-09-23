693c36d48883f323e52a3f675de15127f61c2d94
Mehmood <m.hassan.rahi@gmail.com>
2021-09-23 16:53:18 +0500

    * Removed unnecessary file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import Greet from "./components/Greet";
 
 function App() {
-    return <div className="App">Welcome to React with TypeScript.</div>;
+    return <Greet />
 }
 
 export default App;
